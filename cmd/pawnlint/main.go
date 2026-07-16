37b9ab8563b576dd554e5405c9e6fbd73c4794fb
michael <adrfranklin95@gmail.com>
2026-07-16 08:09:13 +0000

    perf: bound default cli heap growth

--- cmd/pawnlint/main.go
+++ cmd/pawnlint/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"runtime/debug"
 
 	"github.com/pawnkit/pawnlint/internal/cli"
 	"go.uber.org/automaxprocs/maxprocs"
@@ -9,5 +10,8 @@ import (
 
 func main() {
 	_, _ = maxprocs.Set(maxprocs.Logger(func(string, ...any) {}))
+	if os.Getenv("GOMEMLIMIT") == "" {
+		debug.SetMemoryLimit(900 << 20)
+	}
 	os.Exit(cli.Run(os.Args[1:], os.Stdin, os.Stdout, os.Stderr))
 }
