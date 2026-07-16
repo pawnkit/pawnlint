04a25875f4ef9dcf54d20420bbbb0d14e07d4b22
michael <adrfranklin95@gmail.com>
2026-07-16 08:30:09 +0000

    fix: remove forced cli memory limit

--- cmd/pawnlint/main.go
+++ cmd/pawnlint/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"os"
-	"runtime/debug"
 
 	"github.com/pawnkit/pawnlint/internal/cli"
 	"go.uber.org/automaxprocs/maxprocs"
@@ -10,8 +9,5 @@ import (
 
 func main() {
 	_, _ = maxprocs.Set(maxprocs.Logger(func(string, ...any) {}))
-	if os.Getenv("GOMEMLIMIT") == "" {
-		debug.SetMemoryLimit(900 << 20)
-	}
 	os.Exit(cli.Run(os.Args[1:], os.Stdin, os.Stdout, os.Stderr))
 }
